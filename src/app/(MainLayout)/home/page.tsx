2031b8eea9bbb9d7566ab7c8d02f7ec166976382
unknown <raihanshorker56@gmail.com>
2025-03-02 16:16:30 +0600

    complete work section with responsive design

--- src/app/(MainLayout)/home/page.tsx
+++ src/app/(MainLayout)/home/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/app/component/Home/Banner";
 import FunFact from "@/app/component/Home/FunFact";
 import Hero from "@/app/component/Home/Hero";
+import Work from "@/app/component/Home/Work";
 
 const HomePage = () => {
     return (
@@ -8,6 +9,7 @@ const HomePage = () => {
             <Banner/>
             <Hero/>
             <FunFact/>
+            <Work/>
         </div>
     );
 };
