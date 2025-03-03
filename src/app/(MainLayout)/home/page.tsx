dbac71bbfccc479643874974d8d8ecf747fceb67
rayhan108 <raihanshorker56@gmail.com>
2025-03-03 12:02:41 +0600

    added islamic section

--- src/app/(MainLayout)/home/page.tsx
+++ src/app/(MainLayout)/home/page.tsx
@@ -1,7 +1,9 @@
 import Banner from "@/app/component/Home/Banner";
+import Discover from "@/app/component/Home/Discover";
 import FunFact from "@/app/component/Home/FunFact";
 import HadithSlider from "@/app/component/Home/HadithSlider";
 import Hero from "@/app/component/Home/Hero";
+import Islamic from "@/app/component/Home/Islamic";
 import LittleCalips from "@/app/component/Home/LittleCaliphs";
 import TinyMuminsSection from "@/app/component/Home/TinyMumins";
 import Work from "@/app/component/Home/Work";
@@ -16,6 +18,8 @@ const HomePage = () => {
             <HadithSlider/>
             <TinyMuminsSection/>
             <LittleCalips/>
+            <Discover/>
+            <Islamic/>
         </div>
     );
 };
