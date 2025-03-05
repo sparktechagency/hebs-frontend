6c5c9217743c3e7f849ad4a6d8731cae8211cf47
rayhan108 <raihanshorker56@gmail.com>
2025-03-05 22:32:01 +0600

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    logging: 'verbose',
-    appDir: true, // Ensure this is present if you're using the App Router
-  },
+  experimental: {}, // Remove unrecognized keys
 };
 
 module.exports = nextConfig;
