a6dad3cb85f85cea4aaaea3bb154b0f5dc418ee2
rayhan108 <raihanshorker56@gmail.com>
2025-03-05 17:22:54 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // Ensure this is present if you're using the App Router
+  },
 };
 
 module.exports = nextConfig;
