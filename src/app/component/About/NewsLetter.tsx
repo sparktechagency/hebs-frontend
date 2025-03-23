eea4325207772e3f6d707c850de1d8c53efb31da
rayhan108 <raihanshorker56@gmail.com>
2025-03-23 11:04:45 +0600

    fixed about page newsletter

--- src/app/component/About/NewsLetter.tsx
+++ src/app/component/About/NewsLetter.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import Image from "next/image";
-import { Input, Button } from "antd";
+import { Input} from "antd";
 import style from "@/app/styles.module.css";
 import img1 from "@/assets/Image.png";
 import img2 from "@/assets/Image (1).png";
@@ -10,37 +10,37 @@ import tab from "@/assets/tab.png";
 
 export default function NewsletterSignup() {
   return (
-    <div className="flex items-center justify-center bg-[#ffffff] px-4 py-16 relative">
-      <div className="bg-[#FEECDE] w-full max-w-7xl flex flex-col md:flex-row items-center relative p-6 rounded-lg shadow-lg">
+    <div className="flex items-center justify-center bg-[#ffffff] py-16 relative">
+      <div className="bg-[#FEECDE] p-8 md:w-[800px]  max-w-7xl flex flex-col md:flex-row items-center relative rounded-lg shadow-lg  ">
         {/* Left Book Image (Hidden on small screens) */}
-        <div className="absolute left-0 mb:-left-36 lg:-left-36 xl:-left-36 bottom-0 hidden md:block w-[180px] lg:w-[250px]">
+        <div className="absolute -left-80 bottom-0 hidden md:block ">
           <Image
             src={img1}
             alt="My Prophet Muhammad - My Hero"
-            width={250}
-            height={350}
-            className="object-contain rotate-[-15deg]"
+            width={350}
+            height={470}
+            className="object-contain rotate-[3deg]"
           />
         </div>
 
         {/* Center Content */}
-        <div className="flex flex-col md:flex-row items-center w-full">
+        <div className="flex flex-col md:flex-row items-center w-full ">
           {/* Image Section (Tablet in the center) */}
           <div className="w-full md:w-1/2 flex justify-center">
-            <div className="p-2 rounded-lg">
+            <div className="p-2 rounded-lg border-8 border-black">   
               <Image
                 src={tab}
                 alt="Newsletter Promo"
                 className="rounded-lg"
-                height={350}
-                width={250}
+                height={450}
+                width={450}
               />
             </div>
           </div>
 
           {/* Form Section */}
           <div className="w-full md:w-1/2 px-4 py-8 md:px-6 md:py-10 text-center md:text-left">
-            <h2 className={`text-2xl md:text-3xl font-semibold text-gray-900 ${style.fontJost}`}>
+            <h2 className={`text-3xl md:text-6xl  text-[#F37975] ${style.fontJost}`}>
               JOIN OUR NEWSLETTER.
             </h2>
             <p className={`${style.fontPoppins} text-gray-700 mt-2`}>
@@ -59,20 +59,20 @@ export default function NewsletterSignup() {
               <Input placeholder="Enter your first name" className="mt-1 rounded-md" />
             </div>
 
-            <Button className={`mt-6 bg-[#111111] border-none text-white w-full md:w-auto px-6 py-2 rounded-md ${style.fontPoppins}`}>
+            <button className={`mt-6 bg-[#111111] border-none text-white w-full md:w-auto px-6 py-2 rounded-md ${style.fontPoppins}`}>
               Subscribe
-            </Button>
+            </button>
           </div>
         </div>
 
         {/* Right Book Image (Hidden on small screens) */}
-        <div className="absolute right-0 lg:-right-48 xl:-right-48 mb:-right-48 bottom-0 hidden md:block w-[180px] lg:w-[250px]">
+        <div className="absolute -right-72 bottom-0 hidden md:block">
           <Image
             src={img2}
             alt="Planet Omar"
-            width={250}
-            height={350}
-            className="object-contain"
+            width={350}
+            height={470}
+             className="object-contain rotate-[-5deg]"
           />
         </div>
       </div>
