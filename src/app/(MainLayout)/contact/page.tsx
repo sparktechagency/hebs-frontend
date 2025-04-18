d6cbb71cce15773f6cc94fc41b24dc1c90522326
rayhan108 <raihanshorker56@gmail.com>
2025-04-18 20:06:09 +0600

    update contact page text size

--- src/app/(MainLayout)/contact/page.tsx
+++ src/app/(MainLayout)/contact/page.tsx
@@ -17,30 +17,47 @@ const ContactPage = () => {
   return (
     <div>
       <div className="flex justify-center bg-[#FEECDE]">
-        <div className="w-full md:w-1/2 px-4 py-6 md:px-6 md:py-8 lg:py-10 xl:py-12 text-center md:text-left  container mx-auto ">
-     <div className="">
-     <h2 className={`text-6xl font-bold text-[#FCB876] ${style.fontRozha}`}>
-            Contact Us
-          </h2>
-          <p className={`text-[#F37975] text-3xl md:text-4xl py-3  mt-2 tracking-wider uppercase  border-b-4 border-black  inline-block ${style.fontJosefin}`}>We&apos;re here to help</p>
-     </div>
-
-          <div className="mt-4">
-            <label className={` block text-[#F37975] font-bold py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}>
-              Email Address <span className="text-red-500">*</span>
-            </label>
-            <Input placeholder="Enter your email" className={`mt-1 rounded-md ${style.fontPoppins}`} />
+        <div className=" md:w-1/2  px-3 py-6 md:px-6 md:py-8 lg:py-10 xl:py-12 text-center md:text-left  container mx-auto ">
+          <div className="">
+            <h2
+              className={` text-5xl sm:text-6xl  text-[#FCB876] ${style.fontRozha}`}
+            >
+              Contact Us
+            </h2>
+            <p
+              className={`text-[#F37975] text-xl sm:text-2xl md:text-4xl py-3  mt-2 tracking-wider uppercase  border-b-4 border-black  inline-block ${style.fontJosefin}`}
+            >
+              We&apos;re here to help
+            </p>
           </div>
 
           <div className="mt-4">
-            <label className={` block text-[#F37975] font-bold py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}>Subject</label>
+            <label
+              className={` block text-[#F37975]  py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}
+            >
+              Email Address <span className="text-red-500">*</span>
+            </label>
+            <Input
+              placeholder="Enter your email"
+              className={`mt-1 rounded-md ${style.fontPoppins}`}
+            />
+          </div>
+
+          <div className="mt-4">
+            <label
+              className={` block text-[#F37975]  py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}
+            >
+              Subject
+            </label>
             <Input
               placeholder="Enter your first name"
               className={`mt-1 rounded-md ${style.fontPoppins}`}
             />
           </div>
           <div className="mt-4">
-            <label className={` block text-[#F37975] font-bold py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}>
+            <label
+              className={` block text-[#F37975]  py-1 tracking-wide md:tracking-widest text-start text-xl md:text-2xl  ${style.fontRozha}`}
+            >
               NAME & ADDRESS OF SUBSCRIBER:
             </label>
             <TextArea
@@ -49,7 +66,9 @@ const ContactPage = () => {
             />
           </div>
           <div className="mt-4">
-            <label className={` block text-[#F37975] font-bold py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}>
+            <label
+              className={` block text-[#F37975]  py-1 tracking-widest text-start text-2xl  ${style.fontRozha}`}
+            >
               Description:
             </label>
             <TextArea
@@ -59,14 +78,20 @@ const ContactPage = () => {
           </div>
 
           <div className="flex justify-end mt-6">
-  <button className={`bg-[#F37975] border-none text-white px-6 py-2 rounded-sm tracking-wider ${style.fontInter}`}>
-    Send
-  </button>
-</div>
+            <button
+              className={`bg-[#F37975] border-none text-white px-6 py-2 rounded-sm tracking-wider ${style.fontInter}`}
+            >
+              Send
+            </button>
+          </div>
 
-          <div className={`bg-[#ffffff] my-5 text-center py-5 `}>
-            <h1 className={`${style.fontJost} py-2 text-xl tracking-widest`}>Don&apos;t hesitate to reach out with any questions.</h1>
-            <p className={`${style.fontPoppins} font-bold py-2`}>INFO@ILLUMINATEMUSLIMMINDS.COM</p>
+          <div className={`bg-[#ffffff] my-5 text-center py-5 px-1 md:px-5`}>
+            <h1 className={`${style.fontJost} py-2 text-xl tracking-widest`}>
+              Don&apos;t hesitate to reach out with any questions.
+            </h1>
+            <p className={`${style.fontPoppins} font-bold py-2`}>
+              INFO@ILLUMINATEMUSLIMMINDS.COM
+            </p>
             <div className="flex space-x-4 mt-2 justify-center items-center py-5">
               <FacebookOutlined className="text-red-400 text-center text-xl" />
               <MailOutlined className="text-red-400 text-center text-xl" />
