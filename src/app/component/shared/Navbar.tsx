8486a8393b779a1f4382b1b8e3d1c44f5bc9f92c
unknown <raihanshorker56@gmail.com>
2025-04-22 17:56:50 +0600

    comment billing section from my profile page

--- src/app/component/shared/Navbar.tsx
+++ src/app/component/shared/Navbar.tsx
@@ -211,12 +211,12 @@ export default function Navbar() {
                   >
                     Billing History
                   </Link>
-                  {/* <Link
+                  <Link
                     href="/favorites"  onClick={() => setProfileDropdownOpen(false)}
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                   >
                     Favorites
-                  </Link> */}
+                  </Link>
                   <div className="border-t border-gray-200 my-1"></div>
                   <button
                     onClick={() => setUser(!user)}
