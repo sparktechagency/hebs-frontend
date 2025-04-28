a7a670e99652105a804f5c803e2d957267959a92
unknown <raihanshorker56@gmail.com>
2025-04-28 22:34:54 +0600

    set token on headers

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,22 +1,23 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { RootState } from "../store";
 // import { RootState } from "../store";
      
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://10.0.60.55:5003/v1",
-    prepareHeaders: (headers) => {
-      headers.set('Accept', 'application/json');
-      return headers;
-    },
-        // credentials: "include",
-    // prepareHeaders: (headers, { getState }) => {
-    //   const token = (getState() as RootState).auth.token;
-    //   if (token) {
-    //     headers.set("authorization", `${token}`);
-    //   }
-    //   return headers;
-    // },
-  });
+  baseUrl: "http://10.0.60.55:5003/v1",
+  prepareHeaders: (headers, { getState }) => {
+    headers.set('Accept', 'application/json');
+
+    // ✅ Get token from Redux store
+    const token = (getState() as RootState).auth.token;
+    
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+  // credentials: "include", // Optional, if you want to send cookies
+});
 
 
 
