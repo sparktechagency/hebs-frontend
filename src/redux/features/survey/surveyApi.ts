66f963f94c895a9ac11a1f01847fe27cd4e298ab
rayhan108 <raihanshorker56@gmail.com>
2025-05-01 17:27:53 +0600

    call servey api

--- src/redux/features/survey/surveyApi.ts
+++ src/redux/features/survey/surveyApi.ts
@@ -3,10 +3,10 @@ import { baseApi } from "../../api/baseApi";
 const surveyApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: (userInfo) => ({
-        url: "/user/auth/login",
+      query: (info) => ({
+        url: "/survey/create",
         method: "POST",
-        body: userInfo,
+        body: info,
       }),
     }),
    
