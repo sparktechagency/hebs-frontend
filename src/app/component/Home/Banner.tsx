29db2ab618ec1f969a0b83f75841f27b12ef5e16
unknown <raihanshorker56@gmail.com>
2025-05-25 16:36:24 +0600

    fix import issues

--- src/app/component/Home/Banner.tsx
+++ src/app/component/Home/Banner.tsx
@@ -6,17 +6,18 @@ import styles from "@/app/styles.module.css";
 import frame1 from "@/assets/tinyMuminsFrame1.png"
 
 import { useCreateSubscriptionMutation } from "@/redux/features/subscription/subscriptionApi";
-import { useRouter } from "next/router";
+
 import { useAppSelector } from "@/redux/hooks";
 import { selectCurrentUser } from "@/redux/features/auth/authSlice";
 import { selectCurrentPlan } from "@/redux/features/subscription/subscriptionSlice";
 import { message } from "antd";
+import { useRouter } from "next/navigation";
 
 
 const Banner = () => {
   // bg-[#FDFBDF]
-    const [createSubscription] = useCreateSubscriptionMutation();
   const router = useRouter();
+    const [createSubscription] = useCreateSubscriptionMutation();
   const user = useAppSelector(selectCurrentUser);
     const plan = useAppSelector(selectCurrentPlan);
 const handleSurvey=async()=>{
